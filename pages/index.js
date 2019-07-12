96e6ed9bcb420e70bbbb8a5f9c49ff9b5f911a80
mrgawde <mehulgawdenxt@gmail.com>
2019-07-12 12:28:53 +0530

    Koa Next Base Setup

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,15 @@ import 'isomorphic-unfetch'
 
 export default class HomePage extends React.Component {
     static async getInitialProps() {
-        let res = await fetch('https://cloudreports.net/sample/api/countries.json')
-        let contriesObj = await res.json()
-        console.log(contriesObj)
-        return {countries: contriesObj}
+        // let res = await fetch('Some Data From Server')
+        // let jsonObj = await res.json()
+        // return {jObj: jsonObj}
     }
 
     render() {
         return (
             <div>
-                Hello World.<br />
-                <Link href='/about'><a>About</a></Link>
-                <h2>Country list</h2>
-                <ul>
-                    {this.props.countries.map((country, i) => {
-                        return (
-                            <li key={'country-' + i}>{country.name}</li>
-                        )
-                    })}
-                </ul>
+                Hello Next+Koa
             </div>
         )
     }
